0c20cac7896d81d89565f142441ae74d2b8a39bc
Jailine <jay.santana964@gmail.com>
2023-04-25 16:30:50 -0300

    criação do projeto Spring/JPA

---
+++ projetoJpaSpring/src/test/java/com/projeto02/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto02.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
