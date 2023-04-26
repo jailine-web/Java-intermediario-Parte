15ea3d9d73e61a9284e31b91f2ca68d85b836248
Jailine <jay.santana964@gmail.com>
2023-04-26 16:56:59 -0300

    Camada de serviço, registrando os componentes

---
+++ projetoJpaSpring/src/main/java/com/projeto02/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.projeto02.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto02.curso.entities.User;
+import com.projeto02.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired // para que o Spring faça a injeção de dependência de forma transparente ao programador
+	private UserRepository repository;
+	
+	//busca todos os usuarios
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
