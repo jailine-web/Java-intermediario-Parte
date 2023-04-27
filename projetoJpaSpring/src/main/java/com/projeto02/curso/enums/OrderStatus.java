91c8d360d39f47a3e86accf6195f3ffbc481261d
Jailine <jay.santana964@gmail.com>
2023-04-27 15:01:26 -0300

    Maneira correta de implementar o Enum (Estado do pedido)

--- projetoJpaSpring/src/main/java/com/projeto02/curso/enums/OrderStatus.java
+++ projetoJpaSpring/src/main/java/com/projeto02/curso/enums/OrderStatus.java
@@ -2,9 +2,30 @@ package com.projeto02.curso.enums;
 
 public enum OrderStatus {
 
-	WAITING_PAYMENT,
-	PAID,
-	DELIVERED,
-	CANCELED;
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
 	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	//tornar o atributo visivel
+	public int getCode() {
+		return code;
+	}
+	
+	//Converter um tipo numerico em um valor numerado
+	public static OrderStatus valueOf(int code) {
+		for(OrderStatus value: OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Estado do pedido inválido");
+	}
 }
