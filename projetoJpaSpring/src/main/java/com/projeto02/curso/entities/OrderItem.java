9c43c1b8f8df2c36d2d92b22e511811baca833da
Jailine <jay.santana964@gmail.com>
2023-05-02 16:22:14 -0300

    Classe orderItem, relação muitos para muitos- chave composta parte 01

---
+++ projetoJpaSpring/src/main/java/com/projeto02/curso/entities/OrderItem.java
@@ -0,0 +1,84 @@
+package com.projeto02.curso.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.projeto02.curso.entities.PK.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_orderItem")
+public class OrderItem implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId // Para chave-primária
+	private OrderItemPK id;
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Order order, Product product ,Integer quantity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(price, other.price);
+	}
+	
+	
+}
