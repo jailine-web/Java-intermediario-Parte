08e9c580da421afa209b2519fc0b651945288227
Jailine <jay.santana964@gmail.com>
2023-05-03 18:58:24 -0300

    Métodos subtotal e total: Classes order e orderItem

--- projetoJpaSpring/src/main/java/com/projeto02/curso/entities/OrderItem.java
+++ projetoJpaSpring/src/main/java/com/projeto02/curso/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubtotal() {
+		return quantity * price;
+	}
 
 	@Override
 	public int hashCode() {
