5e988482d8c5779c307088db1db19a17e1d9de06
Jailine <jay.santana964@gmail.com>
2023-05-03 18:44:08 -0300

    Entidade Payment e associação um para um

--- projetoJpaSpring/src/main/java/com/projeto02/curso/config/TestConfig.java
+++ projetoJpaSpring/src/main/java/com/projeto02/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projeto02.curso.entities.Category;
 import com.projeto02.curso.entities.Order;
 import com.projeto02.curso.entities.OrderItem;
+import com.projeto02.curso.entities.Payment;
 import com.projeto02.curso.entities.Product;
 import com.projeto02.curso.entities.User;
 import com.projeto02.curso.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2, oi3, oi4));
+		
+		Payment pg1 = new Payment(null,Instant.parse("2019-06-20T19:53:07Z") , o1);
+		o1.setPayment(pg1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
